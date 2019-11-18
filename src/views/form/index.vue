c76ff793c9b137e42d60a758acb1fda0f0256248
dcwhy <1150215282@qq.com>
2019-11-18 16:15:38 +0800

    <fix>:解决代码规范

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="top_option">
-
     <el-select v-model="value" class="why_select" @change="op_click">
       <el-option
         v-for="item in options"
@@ -12,42 +11,18 @@
     </el-select>
 
     <div class="table_div">
-      <el-table
-        v-loading="listLoading"
-        :data="classes"
-        style="width: 100%"
-      >
-        <el-table-column
-          prop="classname"
-          label="班级名称"
-        />
-        <el-table-column
-          prop="createDate"
-          label="创建日期"
-        />
-        <el-table-column
-          prop="major"
-          label="专业"
-        />
-        <el-table-column
-          prop="lecturer"
-          label="讲师"
-        />
-        <el-table-column
-          prop="headteacher"
-          label="班主任"
-        />
-        <el-table-column
-          label="班级成员"
-        >
-        <template slot-scope="scope">
-          <span class="go_student" @click="member(scope.row)">详 情</span>
-        </template>
+      <el-table v-loading="listLoading" :data="classes" style="width: 100%">
+        <el-table-column prop="classname" label="班级名称" />
+        <el-table-column prop="createDate" label="创建日期" />
+        <el-table-column prop="major" label="专业" />
+        <el-table-column prop="lecturer" label="讲师" />
+        <el-table-column prop="headteacher" label="班主任" />
+        <el-table-column label="班级成员">
+          <template slot-scope="scope">
+            <span class="go_student" @click="member(scope.row)">详 情</span>
+          </template>
         </el-table-column>
-        <el-table-column
-          label="操作"
-          min-width="180"
-        >
+        <el-table-column label="操作" min-width="180">
           <template slot-scope="scope">
             <el-button type="primary" size="mini" @click="update(scope.$index, scope.row)">修 改</el-button>
             <el-button type="danger" size="mini" @click="remove(scope.row,scope.row._id)">删 除</el-button>
@@ -55,18 +30,20 @@
         </el-table-column>
       </el-table>
       <!-- 分页 -->
-      
 
-      <el-dialog
-        title="修改操作"
-        :visible.sync="show"
-        width="30%">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-dialog title="修改操作" :visible.sync="show" width="30%">
+        <el-form
+          ref="ruleForm"
+          :model="ruleForm"
+          :rules="rules"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
           <el-form-item label="讲师" prop="lecturer">
-            <el-input v-model="ruleForm.lecturer"></el-input>
+            <el-input v-model="ruleForm.lecturer" />
           </el-form-item>
           <el-form-item label="班主任" prop="headteacher">
-            <el-input v-model="ruleForm.headteacher"></el-input>
+            <el-input v-model="ruleForm.headteacher" />
           </el-form-item>
         </el-form>
         <span slot="footer" class="dialog-footer">
@@ -81,84 +58,101 @@
       title="转移学生"
       :visible.sync="zyStu"
       width="30%"
-      :before-close="handleClose">
+      :before-close="handleClose"
+    >
       <el-table
-      ref="multipleTable"
-      highlight-current-row
-      @current-change="handleCurrentChange"
-      class="allstutab"
-      :data="all"
-      height="300"
-      style="width: 100%">
-        <el-table-column
-          prop="name"
-          label="姓名">
+        ref="multipleTable"
+        highlight-current-row
+        class="allstutab"
+        :data="all"
+        height="300"
+        style="width: 100%"
+        @current-change="handleCurrentChange"
+      >
+        <el-table-column prop="name" label="姓名">
           <template slot-scope="scope">
-            <div style="height:100%">{{scope.row.name}}</div>
+            <div style="height:100%">{{ scope.row.name }}</div>
           </template>
         </el-table-column>
-        <el-table-column
-          prop="chengji"
-          label="成绩">
+        <el-table-column prop="chengji" label="成绩">
           <template slot-scope="scope">
-            <div>{{scope.row.chengji}}</div>
+            <div>{{ scope.row.chengji }}</div>
           </template>
         </el-table-column>
       </el-table>
       <p class="allp">转移到</p>
-      
+
       <div style="position:relative">
-        <el-select style="padding-left:10px" filterable v-model="value1" placeholder="请选择"  @change="changeClass">
+        <el-select
+          v-model="value1"
+          style="padding-left:10px"
+          filterable
+          placeholder="请选择"
+          @change="changeClass"
+        >
           <el-option
             v-for="item in classes"
             :key="item._id"
             :label="item.classname"
             :value="item.classname"
-            :disabled="item.disabled">
-          </el-option>
+            :disabled="item.disabled"
+          />
         </el-select>
-        <br/>
-        <el-button style="margin:0px 10px;width:92%;position:absolute;bottom:0;" type="primary" @click="zyStus">转移</el-button>
+        <br >
+        <el-button
+          style="margin:0px 10px;width:92%;position:absolute;bottom:0;"
+          type="primary"
+          @click="zyStus"
+        >转移</el-button>
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button size="small" @click="zyStu = false">关 闭</el-button>
       </span>
     </el-dialog>
-    
   </div>
 </template>
 
 <script>
 // 引入接口函数
-import { getClass, delClass, updateClass, allstudent, getUpdate } from '../../api/api.js'
+import {
+  getClass,
+  delClass,
+  updateClass,
+  allstudent,
+  getUpdate
+} from '../../api/api.js';
 export default {
   data() {
     return {
-      zyStu : false,
+      zyStu: false,
       listLoading: true,
       show: false, // 弹窗
       classes: [], // 获取所有班级
-      allstudent:[], //获取所有学生
-      classstudents:[],
+      allstudent: [], // 获取所有学生
+      classstudents: [],
       expands: [], // 要展开的行，数值的元素是row的key值
-      xzmajor:"", //选择专业
-      options: [{
-        value: '全部班级',
-        label: '全部班级'
-      }, {
-        value: 'WEB架构',
-        label: 'WEB架构'
-      }, {
-        value: '视觉设计',
-        label: '视觉设计'
-      }],
+      xzmajor: '', // 选择专业
+      options: [
+        {
+          value: '全部班级',
+          label: '全部班级'
+        },
+        {
+          value: 'WEB架构',
+          label: 'WEB架构'
+        },
+        {
+          value: '视觉设计',
+          label: '视觉设计'
+        }
+      ],
       value: '全部班级',
       value1: '全部班级',
       path: '/form/classstudent',
       ruleForm: {
-        lecturer:"",
-        headteacher:"",
-        id:""
+        lecturer: '',
+        headteacher: '',
+        id: ''
       },
       rules: {
         lecturer: [
@@ -170,14 +164,14 @@ export default {
           { min: 2, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
         ]
       },
-      rowlist:[], //修改旧值
-      all:[], //班级内的学生
-      zystuclass:[], //转移学生
-      play: false, //控制显示隐藏
-      changeStuClass:'', //转移选择班级
-      zyStuId:'', //学生id
-      multipleSelection: "",
-      currentRow: null,
+      rowlist: [], // 修改旧值
+      all: [], // 班级内的学生
+      zystuclass: [], // 转移学生
+      play: false, // 控制显示隐藏
+      changeStuClass: '', // 转移选择班级
+      zyStuId: '', // 学生id
+      multipleSelection: '',
+      currentRow: null
     }
   },
   async mounted() {
@@ -194,80 +188,82 @@ export default {
       }
     },
     // 删除班级
-    async remove(e,id) {
+    async remove(e, id) {
       const { data } = await allstudent()
       this.allstudent = data.data
       const classstudents = this.classstudents
-      for(var i = 0;i<this.allstudent.length;i++){
-        if(e.classname === this.allstudent[i].classes){
+      for (var i = 0; i < this.allstudent.length; i++) {
+        if (e.classname === this.allstudent[i].classes) {
           classstudents.push(this.allstudent[i])
         }
       }
       this.allstudent = classstudents
-      localStorage.setItem("datas",JSON.stringify(this.allstudent))
-      this.all = JSON.parse(localStorage.getItem('datas'));
+      localStorage.setItem('datas', JSON.stringify(this.allstudent))
+      this.all = JSON.parse(localStorage.getItem('datas'))
       console.log(this.all)
       // 判断班里是否有学生
-      if(this.all[0]){
+      if (this.all[0]) {
         // 若有学生，转移学生
         const h = this.$createElement
         this.$msgbox({
           title: '提示',
-          message: h('p', null, [
-            h('span', null, '删除班级前请先转移学生')
-          ]),
+          message: h('p', null, [h('span', null, '删除班级前请先转移学生')]),
           showCancelButton: true,
           confirmButtonText: '转移',
           cancelButtonText: '取消',
           type: 'warning'
-        }).then(async res => {
-          
-          this.zyStu = true
-        }).catch(err => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
         })
+          .then(async res => {
+            this.zyStu = true
+            // eslint-disable-next-line handle-callback-err
+          })
+          // eslint-disable-next-line handle-callback-err
+          .catch(err => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          })
         this.classstudents = []
-      }else{
+      } else {
         // 若没有学生,直接删除
         const h = this.$createElement
         this.$msgbox({
           title: '提示',
-          message: h('p', null, [
-            h('span', null, '您确定要移除这个班吗？')
-          ]),
+          message: h('p', null, [h('span', null, '您确定要移除这个班吗？')]),
           showCancelButton: true,
           confirmButtonText: '删除',
           cancelButtonText: '取消',
           type: 'warning'
-        }).then(async res => {
-          const { data } = await delClass(id)
-          console.log(data)
-          if (data.code === 200) {
-            this.handlegetHeadTeacher()
-            this.op_click(this.xzmajor)
-            return this.$message.success(data.msg)
-          }
-          this.$message({
-            message: data.msg,
-            type: 'error'
-          })
-        }).catch(err => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
         })
+          .then(async res => {
+            const { data } = await delClass(id)
+            console.log(data)
+            if (data.code === 200) {
+              this.handlegetHeadTeacher()
+              this.op_click(this.xzmajor)
+              return this.$message.success(data.msg)
+            }
+            this.$message({
+              message: data.msg,
+              type: 'error'
+            })
+            // eslint-disable-next-line handle-callback-err
+          })
+          // eslint-disable-next-line handle-callback-err
+          .catch(err => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          })
       }
-      
     },
     // 班级成员
     async member(e) {
-      localStorage.setItem("data",JSON.stringify(e.classname))
+      localStorage.setItem('data', JSON.stringify(e.classname))
       this.$router.push({
-        path: this.path, // 跳转路由
+        path: this.path // 跳转路由
       })
     },
     // 选择专业
@@ -288,90 +284,88 @@ export default {
     // 修改
     update(index, row) {
       console.log(row)
-      this.rowlist = row;
-      this.show = true;
-      this.ruleForm.lecturer = row.lecturer;
-      this.ruleForm.headteacher = row.headteacher;
-      this.ruleForm.id = row._id;
+      this.rowlist = row
+      this.show = true
+      this.ruleForm.lecturer = row.lecturer
+      this.ruleForm.headteacher = row.headteacher
+      this.ruleForm.id = row._id
     },
     // 确定修改
     async submitForm() {
-      let obj = {
-        _id:this.ruleForm.id,
-        lecturer:this.ruleForm.lecturer,
-        headteacher:this.ruleForm.headteacher
+      const obj = {
+        _id: this.ruleForm.id,
+        lecturer: this.ruleForm.lecturer,
+        headteacher: this.ruleForm.headteacher
       }
       console.log(obj)
-      let { data } = await updateClass(obj);
-      if(
+      const { data } = await updateClass(obj)
+      if (
         obj.lecturer === this.rowlist.lecturer &&
         obj.headteacher === this.rowlist.headteacher
-      ){
+      ) {
         this.$message.success('没有任何修改')
-        this.show = false;
-      }
-      else if (data.code === 200) {
+        this.show = false
+      } else if (data.code === 200) {
         this.handlegetHeadTeacher()
-        this.$message.success("修改成功")
-        this.show = false;
-      }
-      else {
-        this.$message.error(data.msg);
-        return false;
+        this.$message.success('修改成功')
+        this.show = false
+      } else {
+        this.$message.error(data.msg)
+        return false
       }
       this.op_click(this.xzmajor)
     },
     // 取消修改
     secede(formName) {
-      this.$refs[formName].resetFields();
+      this.$refs[formName].resetFields()
       this.$message({
         type: 'info',
         message: '已取消修改'
       })
-      this.show = false;
+      this.show = false
     },
     handleClose(done) {
       this.$confirm('确认关闭？')
-      .then(_ => {
-        done();
-      })
-      .catch(_ => {});
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
     },
     // 表格项
     handleCurrentChange(rows) {
       console.log(rows)
-      this.zyStuId = rows;
+      this.zyStuId = rows
     },
     // 选择的班级
-    changeClass(vel){
+    changeClass(vel) {
       this.changeStuClass = vel
       console.log(this.classes)
     },
     // 点击转移
-    async zyStus(){
-      if(this.zyStuId === ""){
+    async zyStus() {
+      if (this.zyStuId === '') {
         this.$message({
           type: 'info',
           message: '请选择学生'
         })
         return false
       }
-      if(this.changeStuClass === ""){
+      if (this.changeStuClass === '') {
         this.$message({
           type: 'info',
           message: '请选择班级'
         })
         return false
       }
-      let obj = {
-        classes:this.changeStuClass
+      const obj = {
+        classes: this.changeStuClass
       }
       const _id = this.zyStuId._id
-      const success = await getUpdate( _id, obj )
-      if(success.data.code === 200){
+      const success = await getUpdate(_id, obj)
+      if (success.data.code === 200) {
         const { data } = await allstudent()
         this.allstudent = data.data
-        if(this.all[0].classes === this.changeStuClass){
+        if (this.all[0].classes === this.changeStuClass) {
           this.$message({
             type: 'info',
             message: '不能转移到本班'
@@ -379,18 +373,18 @@ export default {
           return false
         } else {
           var num = this.all.indexOf(this.zyStuId)
-          this.all.splice(num,1)
-          this.$message.success("转移成功！")
-          this.zyStuId = ""
+          this.all.splice(num, 1)
+          this.$message.success('转移成功！')
+          this.zyStuId = '';
         }
       }
       this.op_click(this.xzmajor)
-    },
+    }
   }
 }
 </script>
 
 <style scoped>
-   @import "./asset.scss";
+@import "./asset.scss";
 </style>
 
