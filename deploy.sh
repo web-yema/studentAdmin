5ecc4ef7e791d601bd964682ec5a6cd161f2f33d
woyaowanyasuo <2396322386@qq.com>
2019-12-11 15:16:40 +0800

    新增自动提交代码

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-git pull 
-echo '拉取完成'
 git add -A
 git commit -m $1
 echo '自动发布'
 git push origin ly
 git checkout master
+git pull 
+echo '拉取完成'
 git merge ly
 git push origin master
 echo '自动发布完成'
