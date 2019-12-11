97571d00f0a7d5cc04beec76e053ffe4dd99861e
woyaowanyasuo <2396322386@qq.com>
2019-12-11 16:33:31 +0800

    新增自动提交代码功能

--- push.sh
+++ push.sh
@@ -5,13 +5,10 @@ git add -A
 git commit -m $1
 echo '自动发布'
 git push origin ly
-echo '切换主分支'
 git checkout master
-git pull 
+git pull
 echo '拉取完成'
 git merge ly
-git add -A
-git commit -m $1
 git push origin master
 git checkout ly
 echo '自动发布完成'
